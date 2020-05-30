3f41d90c512e83d92e920b06adb0114dec5b1e78
Jaime Lovera <lovera.jaime@gmail.com>
2020-05-29 23:01:20 -0700

    setup routes and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import home from "./pages/home";
+import projects from "./pages/projects";
+import contact from "./pages/contact";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="app-outer-container">
+      <Router>
+        <Navbar />
+        <div className="app-inner-container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/projects" component={projects} />
+            <Route exact path="/contact" component={contact} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from home</h1>
+      </div>
+    );
+  }
+}
+export default home;

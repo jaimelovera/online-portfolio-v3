43e01afb820ddc02845e45612db2680eeed013df
Jaime Lovera <lovera.jaime@gmail.com>
2020-06-01 02:27:43 -0700

    final touches on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import logoImg from "../img/name-logo.png";
 import resume from "../pdf/jaimelovera-resume.pdf";
 import HideOnScroll from "../util/HideOnScroll";
@@ -35,20 +35,21 @@ class Navbar extends Component {
 
     const navLinks = (linkStyles, linksContainerStyles, mobileView = false) => (
       <div className={linksContainerStyles}>
-        {["#about", "#projects", "#contact"].map((id) => {
-          /* Use the anchor tag to get the title of the link */
-          let title = id.slice(1);
-          title = title.charAt(0).toUpperCase() + title.slice(1);
+        {[
+          ["#about", "About"],
+          ["#projects", "Projects"],
+          ["#contact", "Contact"],
+        ].map((item) => {
           return (
             <Link
-              key={id}
-              href={id}
+              key={item[0]}
+              href={item[0]}
               onClick={mobileView ? this.handleDrawerOpenClose : null}
               color="secondary"
               underline="none"
               className={linkStyles}
             >
-              {title}
+              {item[1]}
             </Link>
           );
         })}
@@ -71,57 +72,54 @@ class Navbar extends Component {
     );
 
     return (
-      <Fragment>
-        <HideOnScroll>
-          <div>
-            <AppBar color="primary">
-              <Toolbar className={styles.navContainer}>
-                {/* Empty onOpen function to remove PropType isRequired error. I dont need it... */}
-                <SwipeableDrawer
-                  anchor={"right"}
-                  open={drawerOpen}
-                  onClose={this.handleDrawerOpenClose}
-                  onOpen={() => {}}
+      <HideOnScroll>
+        <AppBar color="primary">
+          <Toolbar className={styles.navContainer}>
+            {/* Empty onOpen function to remove PropType isRequired error.
+            I dont need it... */}
+            <SwipeableDrawer
+              anchor={"right"}
+              open={drawerOpen}
+              onClose={this.handleDrawerOpenClose}
+              onOpen={() => {}}
+            >
+              <Grid
+                container
+                direction="row"
+                justify="flex-end"
+                alignItems="center"
+                className={styles.drawerTopBar}
+              >
+                <IconButton
+                  className={styles.menuButton}
+                  onClick={this.handleDrawerOpenClose}
                 >
-                  <Grid
-                    container
-                    direction="row"
-                    justify="flex-end"
-                    alignItems="center"
-                    className={styles.drawerTopBar}
-                  >
-                    <IconButton
-                      className={styles.menuButton}
-                      onClick={this.handleDrawerOpenClose}
-                    >
-                      <CloseIcon />
-                    </IconButton>
-                  </Grid>
-                  {navLinks(
-                    `${styles.navLink} ${styles.navLinkDrawer}`,
-                    `${styles.navLinks} ${styles.navLinksDrawer}`,
-                    true
-                  )}
-                </SwipeableDrawer>
-                <img src={logoImg} alt="jl logo" className={styles.logoImg} />
-                <Hidden xsDown>
-                  {navLinks(styles.navLink, styles.navLinks)}
-                </Hidden>
-                <Hidden smUp>
-                  {!drawerOpen && (
-                    <IconButton
-                      className={styles.menuButton}
-                      onClick={this.handleDrawerOpenClose}
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                  )}
-                </Hidden>
-              </Toolbar>
-            </AppBar>
-          </div>
-        </HideOnScroll>
-      </Fragment>
+                  <CloseIcon />
+                </IconButton>
+              </Grid>
+              {navLinks(
+                `${styles.navLink} ${styles.navLinkDrawer}`,
+                `${styles.navLinks} ${styles.navLinksDrawer}`,
+                true
+              )}
+            </SwipeableDrawer>
+            <a href="/">
+              <img src={logoImg} alt="Logo" className={styles.logoImg} />
+            </a>
+            <Hidden xsDown>{navLinks(styles.navLink, styles.navLinks)}</Hidden>
+            <Hidden smUp>
+              {!drawerOpen && (
+                <IconButton
+                  className={styles.menuButton}
+                  onClick={this.handleDrawerOpenClose}
+                >
+                  <MenuIcon />
+                </IconButton>
+              )}
+            </Hidden>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
     );
   }
 }

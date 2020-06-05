f2dfef5043f8c84a4885bd79102ad4dde6566f42
Jaime Lovera <lovera.jaime@gmail.com>
2020-06-04 23:55:37 -0700

    added a modal dialog to projects. used a hook instead of a class

--- src/components/sections/projects/ProjectCardGridItem.js
+++ src/components/sections/projects/ProjectCardGridItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 // Material-UI Stuff
@@ -12,6 +12,12 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import CloseIcon from "@material-ui/icons/Close";
 
 // Icons
 import GitHubIcon from "@material-ui/icons/GitHub";
@@ -19,15 +25,50 @@ import LaunchIcon from "@material-ui/icons/Launch";
 
 const styles = (theme) => ({
   card: {},
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
 });
 
 function ProjectCard(props) {
   const { classes, image, title, body, visitLink, gitHubLink } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const visitLinks = (gitHubLink, visitLink) => {
+    return (
+      <Fragment>
+        <IconButton href={gitHubLink} target="_blank">
+          <Tooltip title="GitHub" placement="top" arrow>
+            <GitHubIcon />
+          </Tooltip>
+        </IconButton>
+        <IconButton href={visitLink} target="_blank">
+          <Tooltip title="Visit" placement="top" arrow>
+            <LaunchIcon />
+          </Tooltip>
+        </IconButton>
+      </Fragment>
+    );
+  };
 
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card className={classes.card}>
-        <CardActionArea href={visitLink} target="_blank">
+        <CardActionArea onClick={handleClickOpen}>
           <CardMedia component="img" image={image} alt={title} />
           <CardContent>
             <Typography gutterBottom variant="h5">
@@ -39,20 +80,30 @@ function ProjectCard(props) {
           </CardContent>
         </CardActionArea>
         <Grid container direction="row" justify="flex-end" alignItems="center">
-          <CardActions>
-            <IconButton href={gitHubLink} target="_blank">
-              <Tooltip title="GitHub" placement="top" arrow>
-                <GitHubIcon />
-              </Tooltip>
-            </IconButton>
-            <IconButton href={visitLink} target="_blank">
-              <Tooltip title="Visit" placement="top" arrow>
-                <LaunchIcon />
-              </Tooltip>
-            </IconButton>
-          </CardActions>
+          <CardActions>{visitLinks(gitHubLink, visitLink)}</CardActions>
         </Grid>
       </Card>
+
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <MuiDialogTitle disableTypography>
+          <Typography variant="h6">{title}</Typography>
+          <IconButton className={classes.closeButton} onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </MuiDialogTitle>
+        <MuiDialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+            ac consectetur ac, vestibulum at eros.
+          </Typography>
+        </MuiDialogContent>
+        <MuiDialogActions>{visitLinks(gitHubLink, visitLink)}</MuiDialogActions>
+      </Dialog>
     </Grid>
   );
 }

92fad08f8839954743ea43716edf0da37ef04305
Jaime Lovera <lovera.jaime@gmail.com>
2020-06-11 02:08:35 -0700

    changed load in animation delay

--- src/components/sections/intro/Intro.js
+++ src/components/sections/intro/Intro.js
@@ -40,7 +40,7 @@ const styles = (theme) => ({
   },
   shortInfoAnimate: {
     animation: "slideFromX 3s forwards",
-    animationDelay: "2.25s",
+    animationDelay: "2s",
   },
   longInfo: {
     maxWidth: 500,
@@ -50,7 +50,7 @@ const styles = (theme) => ({
   },
   longInfoAnimate: {
     animation: "slideFromX 1.5s forwards",
-    animationDelay: "3.75s",
+    animationDelay: "3.5s",
   },
 });
 

89a75d429e0576e0c24b585cd92d18a223a50328
Jaime Lovera <lovera.jaime@gmail.com>
2022-04-15 07:58:55 +0000

    docker scripts

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+# These are set automatically.
 HOST_PROJECT_DIR=$(dirname $(dirname $(readlink -f $0)))
 PROJECT_NAME=$(basename ${HOST_PROJECT_DIR})
 
+# Change if different.
+IMAGE_NAME=${PROJECT_NAME}
+CONTAINER_NAME=${PROJECT_NAME}
+
 echo "Running container..."
 docker run -d --rm --pull=never \
---name ${PROJECT_NAME} \
+--name ${CONTAINER_NAME} \
 --hostname container \
 --user $(id -u):$(id -g) \
 --mount type=bind,source="/etc/group",target="/etc/group" \
 --mount type=bind,source="/etc/passwd",target="/etc/passwd" \
 --mount type=bind,source="/etc/shadow",target="/etc/shadow" \
 --mount type=bind,source="${HOST_PROJECT_DIR}",target="${HOME}/${PROJECT_NAME}" \
---mount type=bind,source="${HOME}/.profile",target="${HOME}/.profile" \
 --mount type=bind,source="${HOME}/.bashrc",target="${HOME}/.bashrc" \
 --mount type=bind,source="${HOME}/.bash_aliases",target="${HOME}/.bash_aliases" \
 --workdir "${HOME}/${PROJECT_NAME}" \
 -p 3000:3000 \
-${PROJECT_NAME}
+${IMAGE_NAME}
 
 if [ $? -eq 0 ]; then
-    docker exec --user root ${PROJECT_NAME} /bin/bash -c "chown ${USER}:${USER} ${HOME}"
+    docker exec --user root ${CONTAINER_NAME} bash -c "chown ${USER}:${USER} ${HOME}"
 fi
 
